34cf0f2abe99e5e56355db72a421de45f0cd3398
Kunal Borkar <kunalborkar2001@gmail.com>
2025-06-09 23:11:49 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,12 +1,12 @@
-// def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-//   withSonarQubeEnv("${SonarQubeAPI}"){
-//       sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-//   }
-// }
-
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-    withSonarQubeEnv("${SonarQubeAPI}") {
-        def scannerHome = tool 'SonarQube Scanner'  // matches the name from your screenshot
-        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -Dsonar.sources=."
-    }
+  withSonarQubeEnv("${SonarQubeAPI}"){
+      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
+  }
 }
+
+// def call(String SonarQubeAPI, String Projectname, String ProjectKey){
+//     withSonarQubeEnv("${SonarQubeAPI}") {
+//         def scannerHome = tool 'SonarQube Scanner'  // matches the name from your screenshot
+//         sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -Dsonar.sources=."
+//     }
+// }
