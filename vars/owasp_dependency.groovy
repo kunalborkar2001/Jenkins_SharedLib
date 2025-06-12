c5a69ad9951e2f3103350cd7c477382edf565096
Kunal Borkar <kunalborkar2001@gmail.com>
2025-06-12 20:38:13 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,9 +1,4 @@
-// def call(){
-//   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-//   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
-// }
-
 def call(){
-  dependencyCheck additionalArguments: '--scan ./ --disableCveKev', odcInstallation: 'OWASP'
+  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
